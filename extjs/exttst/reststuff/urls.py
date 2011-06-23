ba6f49673f2101a778dd8611548d7a83ee8f7aba
Espen A. Kristiansen <post@espenak.net>
2011-06-23 19:19:21 +0200

    Made a somewhat working restful app.

--- extjs/exttst/reststuff/urls.py
+++ extjs/exttst/reststuff/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, url
-from views import UserView
+from views import UserView, UserTable
 
 urlpatterns = patterns('',
     url(r'^user/(\d+)?$', UserView.as_view(), name='reststuff-user'),
+    url(r'^usertable$', UserTable.as_view(), name='reststuff-usertable'),
 )
 
--- extjs/exttst/reststuff/views.py
+++ extjs/exttst/reststuff/views.py
@@ -2,7 +2,7 @@ from django.views.generic import View
 from django.http import HttpResponse, HttpResponseNotFound
 from django.utils import simplejson as json
 from django.forms import ModelForm
-
+from django.shortcuts import render
 from models import User
 
 
@@ -26,26 +26,37 @@ def valid_user(f):
 
 
 class UserView(View):
-    @valid_user
-    def get(self, request, user):
-        data = dict(ud=user.id, first=user.first, last=user.last, email=user.email)
-        return HttpResponse(json.dumps(data))
+    #@valid_user
+    #def get(self, request, user):
+        #data = dict(id=user.id, first=user.first, last=user.last, email=user.email)
+        #return HttpResponse(json.dumps(data))
+
+    def get(self, request, username=None):
+        users = [dict(id=user.id, first=user.first, last=user.last, email=user.email) \
+                for user in User.objects.all()]
+        return HttpResponse(json.dumps(users, indent=2), content_type='application/json')
 
     def post(self, request, username=None):
         """ Create """
         data = json.loads(request.raw_post_data)
         userform = UserForm(data)
         userform.save()
-        return HttpResponse(json.dumps(dict(success=True)))
+        return HttpResponse(json.dumps(dict(success=True)), content_type='application/json')
 
     @valid_user
     def put(self, request, user):
         data = json.loads(request.raw_post_data)
         userform = UserForm(data, instance=user)
         userform.save()
-        return HttpResponse(json.dumps(dict(success=True)))
+        return HttpResponse(json.dumps(dict(success=True)), content_type='application/json')
 
     @valid_user
     def delete(self, request, user):
         user.delete()
-        return HttpResponse(json.dumps(dict(success=True)))
+        return HttpResponse(json.dumps(dict(success=True)), content_type='application/json')
+
+
+
+class UserTable(View):
+    def get(self, request):
+        return render(request, 'usertable.html')
