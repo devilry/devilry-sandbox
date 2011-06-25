f0bd8e05d75da203bacded400e91ed8171067b05
Espen A. Kristiansen <post@espenak.net>
2011-06-25 20:22:21 +0200

    Further cleanup

--- extjs/exttst/reststuff/static/restful.js
+++ extjs/exttst/reststuff/static/restful.js
@@ -235,11 +235,6 @@ Ext.onReady(function(){
 
         // Reset and Submit buttons
         buttons: [{
-            text: 'Reset',
-            handler: function() {
-                this.up('form').getForm().reset();
-            }
-        }, {
             text: 'Submit',
             //formBind: true, //only enabled once the form is valid
             //disabled: true,
@@ -261,21 +256,6 @@ Ext.onReady(function(){
                     //}
                 });
             }
-
-
-            //handler: function() {
-                //var form = this.up('form').getForm();
-                //if (form.isValid()) {
-                    //form.submit({
-                        //success: function(form, action) {
-                            //Ext.Msg.alert('Success', action.result.msg);
-                        //},
-                        //failure: function(form, action) {
-                            //Ext.Msg.alert('Failed', action.result.msg);
-                        //}
-                    //});
-                //}
-            //}
         }]
     });
 
